f6c3e6778e5c704cc6251e7e21455fee9b1dd56f
alessia <alessia.cerami@studio.unibo.it>
2020-11-13 19:10:24 +0100

    Added header

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="beforeHeader">
+    <a to="/"> <img src="https://img.icons8.com/color/64/000000/sprout.png"></a>
+    <p class="text">Conserva e tratta il cibo come se fosse il tuo corpo, ricordando che nel tempo il cibo sarà il tuo corpo.<br/>
+      Cit. BW Richardson </p>
+  </div>
+</template>
+
+<style lang="sass">
+//@import './Header'
+</style>
