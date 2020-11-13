9142eb7051acaa9faf53d2df3d50b5cbc172f23f
alessia <alessia.cerami@studio.unibo.it>
2020-11-13 19:10:47 +0100

    Added footer

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="ct-footer-post">
+    <div class="container">
+      <div class="inner-left">
+        <p class="azienda">Azienda Agricola Giordano Emanuele | Via Chiesa, 466, Formignano(FC), 47522</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="sass">
+//@import './Footer'
+</style>
