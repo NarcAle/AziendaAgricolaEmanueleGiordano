c3e18105ce0b52fce3e08f47528d2373b389fac5
alessia <alessia.cerami@studio.unibo.it>
2020-11-13 21:57:08 +0100

    cosa facciamo added page

---
+++ src/views/Pages/CosaFacciamo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="homeContent container">
+    <div class="block">
+      <div class="content-heading"><h3>I nostri prodotti &nbsp </h3></div>
+      <hr style="background-color: black; height: 1px; border: 0;">
+      <div class="space-image">
+        <img class="img-left" src="../../assets/susine.jpg" height="270"/>
+        <img class="img-left" src="../../assets/prodotti1.jpg" height="270"/>
+        <img class="img-left" src="../../assets/raccolto.jpg" height="270"/>
+      </div>
+      <div class="span">
+        <p class="span">L'azienda offre una vastissima gamma di prodotti, divisi tra piante officinali e non officinali. <br/> 
+          Vengono coltivate piante di uso quotidiano, come il <i>rosmarino</i>,
+          la <i>salvia</i>, la <i>menta</i>, il <i>basilico</i> ed ancora <i>cipolle</i>, <i>carciofi</i>, <i>carote</i> e tanto altro!<br/> E poi ci sono piante meno conosciute e
+          meno usate. Esempi sono <i>dragoncello</i>, <i>parietaria</i>, <i>pilosella</i>, <i>melissa</i>, <i>malva</i>, <i>bardana</i> e altre ancora più particolari. <br/>
+          Ma non sempre quello che ci interessa di una pianta è ciò che raccolgono le altre aziende. Per esempio:
+          <ul class="listCosaFacciamo">
+            <li>Della pianta del carciofo vengono raccolte le foglie. </li>
+            <li>Della pianta della bardana viene raccolta la radice.</li>
+            <li>Della cipolla viene raccolto il seme.</li>
+            <li>...</li>
+          </ul>
+        </p>
+        <p class="span">
+          L'esperienza di Emanuele permette all'azienda di offrire ai clienti anche piante molto difficili da coltivare, come ad esempio il <i>vischio</i>.<br/>
+          Nonostante le avversità del clima e dell'ambiente, non vengono usati prodotti chimici, infatti tutti i nostri prodotti sono biologici.
+        </p>
+      </div>
+      <br/>
+
+      <div class="content-heading"><h3>Processo produttivo &nbsp </h3></div>
+      <hr style="background-color: black; height: 1px; border: 0;">
+      <div class="space-image">
+        <img class="img-left" src="../../assets/lavorazione-terra1.jpg"/>
+        <img class="img-left" src="../../assets/lavorazione-terra2.jpg"/>
+        <img class="img-left" src="../../assets/lavorazione-terra3.jpg"/>
+      </div>
+      <div class="span4">
+        <p>
+          La coltivazione parte dalla lavorazione del terreno, che viene fatta con l'utilizzo di mezzi agricoli o, in alcuni casi, a mano.
+          Successivamente viene fatta la semina, che è quasi sempre fatta a mano. A volte non si parte dai semi ma direttamente dalle piantine.<br/>
+          Poi, durante tutto il periodo di crescita, vengono svolti lavori di controllo e di pulizia attorno alle piante per permettergli di crescere rigogliose.
+          <br/> Ogni pianta ha le sue esigenze che Emanuele conosce bene ed è grazie a lui che l'azienda può vantare di offrire prodotti alla migliore qualità sul mercato.<br/>
+          La raccolta viene fatta esclusivamente a mano. Poi, in base alle richieste dei clienti, le piante verranno lavorate così da poter offrirle sia fresche che secche,
+          intere o triturate. <br/>
+          Ogni terreno viene usato a rotazione per diverse piante in modo che non venga impoverito mai. <br/><br/>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="sass">
+@import './CosaFacciamo'
+</style>
