19e99f920e86e2b9697c6c3e3d4a8e62a5398a9d
alessia <alessia.cerami@studio.unibo.it>
2020-11-13 19:11:16 +0100

    Added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import ChiSiamo from '../views/Pages/chiSiamo.vue'
+import CosaFacciamo from '../views/Pages/CosaFacciamo.vue'
+import Galleria from '../views/Pages/Galleria.vue'
+import Contattaci from '../views/Pages/Contattaci.vue'
+
 
 Vue.use(VueRouter)
 
@@ -11,14 +16,24 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    path: '/chi-siamo',
+    name: 'chi-siamo',
+    component: ChiSiamo
+  },
+  {
+    path: '/cosa-facciamo',
+    name: 'cosa-facciamo',
+    component: CosaFacciamo
+  },
+  {
+    path: '/galleria',
+    name: 'galleria',
+    component: Galleria
+  },
+  {
+    path: '/contattaci',
+    name: 'contattaci',
+    component: Contattaci
   }
 ]
 
