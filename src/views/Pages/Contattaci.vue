7ebb1a05aa4d715d6406fb35c6b24a1a915b4a56
alessia <alessia.cerami@studio.unibo.it>
2020-11-13 22:14:42 +0100

    contattaci page added

--- src/views/Pages/Contattaci.vue
+++ src/views/Pages/Contattaci.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="home_part">
-        <h1 class="sede">Azienda Agricola Giordano Emanuele </h1>
-        <hr>
-        <div class="map_and_description">
-          <div>
-            <h3 class="text-c"><strong>Azienda Agricola Giordano Emanuele</strong> </h3> <br/>
-            <p class="text-c interlinea">Via Chiesa, 466 <br/>
-              Formignano(FC), 47522 <br/>
-              Mail: az.agr.giordano.emanuele@gmail.com <br/>
-              Tel.: 0547 373195
-            </p>
-          </div>
-          <div>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5733.072937403026!2d12.16484612804509!3d44.072279424719454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132ca1891932f34d%3A0x8395c23aaac7105c!2sVia+Chiesa+Formignano%2C+47522+Cesena+FC!5e0!3m2!1sit!2sit!4v1552504894605"
-            width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-          </div>
-
-        </div>
-
-        <div>
-          <h2 class="sede">Hanno parlato di noi: </h2><br/>
-          <ul class="container">
-            <li><a class="text-c" href="http://www.ecor.it/it/prodotti/le-terre-di-ecor/az--agr--emanuele-giordano"> Le Terre di Ecor</a></li><br/>
-            <li><a class="text-c" href="https://www.negozicuorebio.it/e/le-terre-di-ecor/azienda-agricola-giordano-emanuele/"> Cuore Bio </a></li><br/><br/>
-          </ul>
-        </div>
-        <br/><br/><br/>
+    <h1 class="sede">La nostra sede</h1>
+    <hr>
+    <div class="map_and_description">
+      <div>
+        <h3 class="text-c"><strong>Azienda Agricola Giordano Emanuele</strong> </h3> <br/>
+        <p class="text-c interlinea">Via Chiesa, 466 <br/>
+          Formignano(FC), 47522 <br/>
+          Mail: az.agr.giordano.emanuele@gmail.com <br/>
+          Tel.: 0547 373195
+        </p>
       </div>
+      <div class="containerMap">
+        <iframe class="mapAzienda" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5733.072937403026!2d12.16484612804509!3d44.072279424719454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132ca1891932f34d%3A0x8395c23aaac7105c!2sVia+Chiesa+Formignano%2C+47522+Cesena+FC!5e0!3m2!1sit!2sit!4v1552504894605"
+          height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+    </div>
+
+    <div>
+      <h2 class="sede">Hanno parlato di noi: </h2><br/>
+      <ul class="container">
+        <li><a class="text-c" href="http://www.ecor.it/it/prodotti/le-terre-di-ecor/az--agr--emanuele-giordano"> Le Terre di Ecor</a></li><br/>
+        <li><a class="text-c" href="https://www.negozicuorebio.it/e/le-terre-di-ecor/azienda-agricola-giordano-emanuele/"> Cuore Bio </a></li><br/><br/>
+      </ul>
+    </div>
+    <br/><br/><br/>
+  </div>
 </template>
+
+<style lang="sass">
+@import './Contattaci'
+</style>
