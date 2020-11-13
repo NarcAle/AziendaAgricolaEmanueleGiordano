4510bcdf5c55ea2d2051a99c062cd77823a033d2
alessia <alessia.cerami@studio.unibo.it>
2020-11-13 21:31:40 +0100

    chi siamo page added

---
+++ src/views/Pages/chiSiamo.vue
@@ -0,0 +1,32 @@
+<template>
+   <div class="homeContent container">
+      <div class="block chi-siamo">
+        <h1 class="title">La nostra storia</h1>
+        <hr style="background-color: black; height: 1px; border: 0;">
+        <div class="space-image">
+          <img class="img" src="../../assets/emanuele-giordano.jpg"/>
+          <img class="img" src="../../assets//home1.jpg"/>
+        </div>
+        <p class="history">Incorniciata dalle dolci colline che circondano il Cesenate, l'azienda agricola di Emanuele Giordano di Formigliano fa parte della realtà del biologico
+          da più di venticinque anni. <br/>
+          Emanuele iniziò nel 1990 con l'agricoltura tradizionale, ma, nel giro di qualche anno, decise di convertire l'azienda al biologico.
+          L'amore per la natura e per l'attività all'aria aperta gli è stato trasmesso dai suoi genitori, che possedevano un allevamento di bovini,
+          ma Emanuele ha sempre avuto una passione particolare per le piante officinali, che ben si rispecchia nella sua azienda agricola.
+          Questa è composta da circa 22 ettari, dei quali 16 dedicati alla coltivazione delle piante officinali ed i restanti 6 sono utilizzati per un
+          processo di rimboschimento.<br/>
+          Quando nacque l'azienda era però molto più piccola. Infatti, questa, contava solo 3 ettari, che aveva ricevuto in eredità e che nel tempo ha ampliato
+          per poter far crescere il suo sogno.<br/>
+          Nel corso degli anni, Emanuele, ha avuto diversi collaboratori, anche se non sono mai stati più di due o tre contemporaneamente. Alcuni hanno lavorato
+          assieme a lui per periodi molto brevi mentre i più temerari sono rimasti per diversi anni.<br/>
+          Emanuele non vorrebbe fermarsi nemmeno ora: confessa che il suo desiderio è di ampliare ulteriormente l'azienda, soprattutto perché si rende ogni giorno
+          più necessario uno spazio coperto per alcune attività. Ci sono però dei vincoli di regolamenti comunali che lo impediscono, ma il suo frizzante spirito romagnolo
+          troverà il modo per dare più spazio al suo sogno.
+        </p>
+      </div>
+
+    </div>
+</template>
+
+<style lang="sass">
+@import './chiSiamo'
+</style>
